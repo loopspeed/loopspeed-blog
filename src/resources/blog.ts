46039e66ceb2986fcaa8e3bab5f08ae9f6902eeb
Loopspeed Ltd <matthew@loopspeed.co.uk>
2025-07-14 17:44:47 -0700

    Update blog.ts

--- src/resources/blog.ts
+++ src/resources/blog.ts
@@ -7,7 +7,7 @@ import AnimatedCSSGrid, { metadata as animatedCSSGridMetadata} from './posts/ani
 import ImageSequence , {metadata as imageSequenceMetadata } from './posts/image-sequence.mdx'
 import ReactThreeFiberWebGPUTypescript, { metadata as r3fWebGPUMetadata} from './posts/r3f-webgpu-setup.mdx'
 import WavePlane, { metadata as wavePlaneMetadata} from './posts/wave-plane.mdx'
-import NextJsShaders, { metadata as nextShadersMetadata} from './posts/wave-plane.mdx'
+import NextJsShaders, { metadata as nextShadersMetadata} from './posts/next-shaders.mdx'
 
 import type { BlogMetadata } from '@/model/blog'
 
@@ -16,5 +16,4 @@ export const BLOG_CONTENT: Record<BlogSlug, {Component: FC, metadata: BlogMetada
   [BlogSlug.ImageSequenceHeader]: {Component: ImageSequence, metadata: imageSequenceMetadata},
   [BlogSlug.AnimatedCSSGrid]: {Component: AnimatedCSSGrid, metadata: animatedCSSGridMetadata},
   [BlogSlug.ReactThreeFiberWebGPUTypescript]: {Component: ReactThreeFiberWebGPUTypescript, metadata: r3fWebGPUMetadata},
-  [BlogSlug.NextJsShaderSetup]: {Component: NextJsShaders, metadata: nextShadersMetadata}
-}
+  [BlogSlug.NextJsShaderSetup]: {Component: NextJsShaders, metadata: nextShadersMetadata}}
